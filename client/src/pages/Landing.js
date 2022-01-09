db087a81443b15ab1379a87d4961a1cc59404f9a
KhaledAlabssi <khaled.absi5@gmail.com>
2022-01-09 09:40:35 +0100

    landing page + styled components

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import main from '../assets/images/main.svg'
 import logo from '../assets/images/logo.svg'
+import styled from "styled-components"
 
 
 function Landing() {
     return (
-      <main>
+      <Wrapper>
         <nav>
             <img src={logo} alt='logo' className='logo' />
         </nav>
@@ -26,8 +27,50 @@ function Landing() {
           <img src={main} alt='main img' className='img main-img' />
 
         </div>
-      </main>
+      </Wrapper>
     );
 }
 
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+}
+
+.page {
+  min-height: calc(100vh - var(--nav-height));
+  display: grid,
+  align-items: center;
+  margin-top: -3rem;
+}
+
+h1 {
+  font-weight: 700;
+  spam {
+    color: var(--primary-500)
+  }
+}
+
+p {
+  color: var(--grey-600)
+}
+.main-img {
+  display: none;
+}
+ @media (min-width: 992px) {
+   .page {
+     grid-template-columns: 1fr 1fr;
+     column-gap: 3rem;
+   }
+   .main-img {
+     display:block;
+   }
+   
+   
+ }
+`
 export default Landing
