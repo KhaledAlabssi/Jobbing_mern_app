81fbd47e892b83c8685d7470159e0e4aff62d9bc
KhaledAlabssi <khaled.absi5@gmail.com>
2022-01-10 14:19:58 +0100

    app structure + controllers and routes

--- server.js
+++ server.js
@@ -4,15 +4,27 @@ dotenv.config()
 
 const app = express()
 
+// DB
+import connectDB from './db/connect.js'
+
+//routers
+import authRouter from './routes/authRoutes.js'
+import jobsRouter from './routes/jobsRouter.js'
+
 // middleware
 import notFoundMiddleware from './middleware/not_found.js'
 import errorHandlerMiddleware from './middleware/error_handler.js'
-import connectDB from './db/connect.js'
+
+app.use(express.json())
 
 
 app.get('/', (req, res) => {
     res.send('Welcome!!!')
 })
+
+app.use('/api/v1/auth', authRouter)
+app.use("/api/v1/jobs", jobsRouter);
+
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
