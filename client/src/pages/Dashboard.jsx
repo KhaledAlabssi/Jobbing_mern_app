5f5cb4e1653dd7d1a7b8380eff6a8816fa5493b1
KhaledAlabssi <khaled.absi5@gmail.com>
2022-01-16 14:26:10 +0100

    connect frontend to backend w/ proxy

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 function Dashboard() {
+    const fetchData = async () => {
+        try{
+            const response = await fetch('/api/v1')
+            const data = await response.json()
+            console.log(data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
     return (
         <div>
             <h1>Dashboard</h1>
