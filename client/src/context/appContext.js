e6a92f885b7c96aaf17335ae0076a2abfdd80495
KhaledAlabssi <khaled.absi5@gmail.com>
2022-01-18 10:32:36 +0100

    register user front end & local storage

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,43 +1,93 @@
-import React, {useReducer, useContext, createContext } from "react"
-import reducer from "./reducer"
-import { DISPLAY_ALERT, CLEAR_ALERT } from "./action"
+import React, { useReducer, useContext, createContext } from "react";
+import reducer from "./reducer";
+import axios from "axios";
+import {
+  DISPLAY_ALERT,
+  CLEAR_ALERT,
+  REGISTER_USER_BEGIN,
+  REGISTER_USER_ERROR,
+  REGISTER_USER_SUCCESS,
+} from "./action";
 
+const user = localStorage.getItem('user')
+const token = localStorage.getItem("token");
+const userLocation = localStorage.getItem("location");
 
 const initialState = {
-    isLoading: false,
-    showAlert: false,
-    alertText:'',
-    alertType:'',
-}
-const AppContext = React.createContext()
-const AppProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, initialState)
-    const displayAlert = () => {
-        dispatch({type:DISPLAY_ALERT})
-        clearAlert()
+  isLoading: false,
+  showAlert: false,
+  alertText: "",
+  alertType: "",
+  user: user ? JSON.parse(user) : null,
+  token: token,
+  userLocation: userLocation || "",
+  jobLocation: userLocation || "",
+};
+const AppContext = React.createContext();
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
+    clearAlert();
+  };
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({
+        type: CLEAR_ALERT,
+      });
+    }, 3000);
+  };
+
+  const addUserToLocalStorage = ({ user, token, location }) => {
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+    localStorage.setItem("location", location);
+  };
+
+  const removeUserFromLocalStorage = ({ user, token, location }) => {
+    localStorage.removeItem("user", JSON.stringify(user));
+    localStorage.removeItem("token", token);
+    localStorage.removeItem("location", location);
+  };
+
+  const registerUser = async (currentUser) => {
+    dispatch({ type: REGISTER_USER_BEGIN });
+    try {
+      const response = await axios.post("/api/v1/auth/register", currentUser);
+      // console.log(response);
+      const { user, token, location } = response.data;
+      dispatch({
+        type: REGISTER_USER_SUCCESS,
+        payload: { user, token, location },
+      });
+      // localstorage
+      addUserToLocalStorage({ user, token, location });
+    } catch (error) {
+      // console.log(error.response)
+      dispatch({
+        type: REGISTER_USER_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
     }
-    const clearAlert = () => {
-      setTimeout(() => {
-        dispatch({
-          type: CLEAR_ALERT,
-        });
-      }, 3000);
-    };
+    // console.log(currentUser);
+    clearAlert();
+  };
 
-    return (
-        <AppContext.Provider value={{
-            ...state,
-            displayAlert,
-
-        }}
-        >
-            {children}
-        </AppContext.Provider>
-    )
-}
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        displayAlert,
+        registerUser,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
 
 const useAppContext = () => {
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export {AppProvider, initialState, useAppContext}
+export { AppProvider, initialState, useAppContext };
--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -1,4 +1,10 @@
-import { CLEAR_ALERT, DISPLAY_ALERT } from "./action"
+import {
+  CLEAR_ALERT,
+  DISPLAY_ALERT,
+  REGISTER_USER_BEGIN,
+  REGISTER_USER_ERROR,
+  REGISTER_USER_SUCCESS
+} from "./action";
 
 
 const reducer = (state, action) => {
@@ -7,6 +13,28 @@ const reducer = (state, action) => {
             return {...state, showAlert:true, alertType:'danger', alertText:'Please provide all values!'}
         case CLEAR_ALERT:
             return {...state, showAlert:false, alertText:'', alertType:''}
+        case REGISTER_USER_BEGIN:
+          return {...state, isLoading:true}
+        case REGISTER_USER_SUCCESS:
+          return {
+            ...state, 
+            isLoading:false, 
+            token: action.payload.token, 
+            user: action.payload.user,
+            userLocation: action.payload.location,
+            jobLocation: action.payload.location,
+            showAlert: true,
+            alertType: 'success',
+            alertText: 'User Created! Redirecting...'
+          }
+        case REGISTER_USER_ERROR:
+          return {
+            ...state,
+            isLoading: false,
+            showAlert: true,
+            alertType: "danger",
+            alertText: action.payload.msg,
+          };
 
     }
   throw new Error(`no such action :${action.type}`);
