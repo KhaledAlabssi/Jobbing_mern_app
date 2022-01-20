22fd23e1122b54c022e66283d8d530b073a83627
KhaledAlabssi <khaled.absi5@gmail.com>
2022-01-20 12:30:07 +0100

    shared layout/ navbar

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,11 +1,17 @@
 import Logo from './Logo'
 import FormRow from './FormRow'
 import Alert from './Alert'
+import BigSidebar from './BigSidebar'
+import SmallSidebar from './SmallSidebar'
+import Navbar from './Navbar'
 
 
 export {
     Logo,
     FormRow,
     Alert,
+    Navbar,
+    BigSidebar,
+    SmallSidebar,
     
 }
--- client/src/pages/dashboard/SharedLayout.jsx
+++ client/src/pages/dashboard/SharedLayout.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import {Outlet, Link} from 'react-router-dom'
 import Wrapper from '../../assets/wrappers/SharedLayout'
+import {
+  Navbar,
+  SmallSidebar,
+  BigSidebar,
+
+} from '../../components'
 
 function SharedLayout() {
   return <Wrapper>
-    <nav>
-      <Link to='all-jobs'>All Jobs</Link>
-      <Link to='add-job'>Add Jobs</Link>
-    </nav>
-    <Outlet />
+    <main className='dashboard'>
+      <SmallSidebar />
+      <BigSidebar />
+      <div>
+        <Navbar />
+        <div className='dashboard-page'>
+
+          <Outlet />  
+        </div>
+
+      </div>
+    </main>
   </Wrapper>
 }
 
